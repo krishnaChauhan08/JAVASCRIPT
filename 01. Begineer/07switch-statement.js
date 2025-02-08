42addd2f4a1a852d4011565454c1e4de919b5ca5
krishnaChauhan08 <chauhankrishna0820@gmail.com>
2025-02-08 23:19:56 +0530

    switch statement

---
+++ 01. Begineer/07switch-statement.js
@@ -0,0 +1,18 @@
+let fruit = "banana";
+switch (fruit) {
+  case "banana":
+    console.log("Banana is good!");
+    break;
+
+  case "orange":
+    console.log("I am not fan of orange.");
+    break;
+
+  case "apple":
+    console.log("How you like them apples?");
+    break;
+
+  default:
+    console.log("I have never heard of that fruit.");
+    break;
+}
