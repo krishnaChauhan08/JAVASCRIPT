84e698c5a601decfa107cb7be99172204435f767
krishnaChauhan08 <chauhankrishna0820@gmail.com>
2025-02-08 23:11:57 +0530

    consditional statement control flow

---
+++ 01. Begineer/06conditional-statement.js
@@ -0,0 +1,10 @@
+let password;
+if ((password === 8)) {
+  console.log("welcome");
+} else if (password <= 8) {
+  console.log("Password is too short");
+} else if (password >= 8) {
+  console.log("TOo long password and password should be 8 characters");
+} else {
+  console.log("Please provide a password");
+}
