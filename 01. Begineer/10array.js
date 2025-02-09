12dabdaa6e4d22c696f5d61bd34fd597cf66084a
krishnaChauhan08 <chauhankrishna0820@gmail.com>
2025-02-09 10:32:31 +0530

    array in js

---
+++ 01. Begineer/10array.js
@@ -0,0 +1,7 @@
+const favSingers = ["Arijit Singh", "Cammilo Cammela", "Vikash"];
+console.log(favSingers[0]);
+const favNumbers = [7, 20, 21, 25];
+const mixedArr = ["string", ["otherarray"], 123, true];
+for (i = 0; i <= 3; i++) {
+  console.log(mixedArr[i]);
+}
